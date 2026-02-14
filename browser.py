a4c5ab1a8fdba4705e56f3551febf6c0ae41ae0c
Diofbjr <diogofbjr@gmail.com>
2026-02-14 15:15:56 -0300

    update browser 0.1

--- browser.py
+++ browser.py
@@ -5,11 +5,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def configurar_navegador():
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    # ESTAS LINHAS SÃO OBRIGATÓRIAS NO RENDER
+    chrome_options.add_argument("--headless=new") 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
-    # Esse argumento é vital para o Render não dar erro de memória
+    chrome_options.add_argument("--remote-debugging-port=9222")
+    
+    # User agent para não ser bloqueado como bot
     chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
     
     service = Service(ChromeDriverManager().install())
